bfe35cbd95cea334851eb594d03e33d67394e5d6
nowakpawel <kontakt.nowakpawel@gmail.com>
2026-04-02 20:18:45 +0200

    Configure hibernate

--- src/main/java/com/pawel/jwtdemo/jwtdemo/web/controller/UserController.java
+++ src/main/java/com/pawel/jwtdemo/jwtdemo/web/controller/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("user/v1")
+@RestController
+@RequestMapping("user/v1")
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
--- src/main/java/com/pawel/jwtdemo/jwtdemo/web/model/User.java
+++ src/main/java/com/pawel/jwtdemo/jwtdemo/web/model/User.java
@@ -4,7 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public record User(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id, String userName, String email, String password) {
+@Table (name = "jwt_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
 }
